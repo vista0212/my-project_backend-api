b11d62cca1d1afab089dd663ddf13234a52618d2
kb4ek <kbaek1202@gmail.com>
2019-05-30 14:04:06 +0900

    User model 생성

---
+++ database/models/user.ts
@@ -0,0 +1,48 @@
+import {
+  AllowNull,
+  AutoIncrement,
+  BelongsTo,
+  Column,
+  CreatedAt,
+  DataType,
+  ForeignKey,
+  Model,
+  PrimaryKey,
+  UpdatedAt,
+  Table,
+  HasMany,
+  Default,
+} from 'sequelize-typescript';
+
+@Table({
+  timestamps: true,
+})
+export default class User extends Model<User> {
+  @AllowNull(false)
+  @PrimaryKey
+  @Default(DataType.UUIDV4)
+  @Column(DataType.UUID)
+  public pk: string;
+
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  public id: string;
+
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  public password: string;
+
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  public passwordKey: string;
+
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  public name: string;
+
+  @CreatedAt
+  public createdAt: Date;
+
+  @UpdatedAt
+  public updatedAt: Date;
+}
