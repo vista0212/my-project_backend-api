980aa280cff9778fbd26cd3cf75bc1d3d8785b31
kb4ek <kbaek1202@gmail.com>
2019-06-02 02:48:35 +0900

    Post model 생성

---
+++ database/models/post.ts
@@ -0,0 +1,52 @@
+import {
+  AllowNull,
+  AutoIncrement,
+  BelongsTo,
+  Column,
+  CreatedAt,
+  DataType,
+  ForeignKey,
+  Model,
+  PrimaryKey,
+  UpdatedAt,
+  Table,
+  HasMany,
+  Default,
+} from 'sequelize-typescript';
+import User from './user';
+
+@Table({
+  timestamps: true,
+})
+export default class Post extends Model<Post> {
+  @PrimaryKey
+  @AutoIncrement
+  @Column(DataType.INTEGER)
+  public pk: number;
+
+  @ForeignKey(() => User)
+  @AllowNull(false)
+  @Column(DataType.UUID)
+  public userPk: string;
+
+  @Column(DataType.STRING)
+  public title: string;
+
+  @Column(DataType.TEXT)
+  public content: string;
+
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  public author: string;
+
+  @CreatedAt
+  public createdAt: Date;
+
+  @UpdatedAt
+  public updatedAt: Date;
+
+  @BelongsTo(() => User, {
+    onDelete: 'CASCADE',
+  })
+  public user: User;
+}
