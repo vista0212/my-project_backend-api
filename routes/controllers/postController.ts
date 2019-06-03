526f5f90e2933b95af9d05984d4fa9f7c8d7fe66
kb4ek <kbaek1202@gmail.com>
2019-06-03 23:13:31 +0900

    Post Delete Router 연결

--- routes/controllers/postController.ts
+++ routes/controllers/postController.ts
@@ -3,13 +3,19 @@ import { Router } from 'express';
 import verifyToken from '../middleware/jwt/verifyToken';
 import postValidation from '../middleware/post/_validation';
 import checkValidation from '../middleware/common/checkValidation';
+
+import findPost from '../middleware/post/common/findPost';
+
 import createPost from '../middleware/post/createPost';
+import checkUser from '../middleware/post/common/checkUser';
+import deletePost from '../middleware/post/deletePost';
 
 const router = Router();
 
 router.use(verifyToken);
 
 router.post('/write', postValidation);
+router.post('/delete', findPost, checkUser, deletePost);
 
 router.use(checkValidation);
 
