53019f0f39ab885aa164949352d4135e1a5833b3
kb4ek <kbaek1202@gmail.com>
2019-06-03 13:21:50 +0900

    비밀번호 변경 API

---
+++ routes/middleware/user/change/password/changePassword.ts
@@ -0,0 +1,42 @@
+import { NextFunction, Request, Response } from 'express';
+
+import User from '../../../../../database/models/user';
+
+const changePassword = async (req: Request, res: Response, next: NextFunction) => {
+  const user: User = res.locals.user;
+  const temp = res.locals.temp;
+
+  try {
+    await User.update(
+      {
+        password: temp.password,
+        passwordKey: temp.passwordKey,
+      },
+      {
+        where: {
+          pk: user.pk,
+        },
+      }
+    );
+
+    res.status(200).json({
+      result: {
+        SUCCESS: true,
+        message: '비밀번호가 변경되었습니다.',
+      },
+      data: {
+        passsword: temp.password,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      result: {
+        SUCCESS: false,
+        message: 'Server Error',
+      },
+    });
+  }
+};
+
+export default changePassword;
