ee156c035b284db4445aad37492537d8b3365a4e
kb4ek <kbaek1202@gmail.com>
2019-06-03 13:19:22 +0900

    유저 정보 변경 기능 추가

---
+++ routes/controllers/updateUserInfo/updateUserController.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+
+import verifyToken from '../../middleware/jwt/verifyToken';
+
+import passwordValidation from '../../middleware/user/change/password/_validation';
+
+import checkValidation from '../../middleware/common/checkValidation';
+import passwordEncryption from '../../middleware/user/common/passwordEncryption';
+
+import changePassword from '../../middleware/user/change/password/changePassword';
+
+const router = Router();
+
+router.use(verifyToken);
+
+router.post('/password', passwordValidation, checkValidation, passwordEncryption, changePassword);
+
+export default Router;
